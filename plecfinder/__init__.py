dab1ed13eda4a8c5294ee7ba3c10a2fb11f57121
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2023-06-28 12:00:58 +0200

    added load_topol to __init__

--- plecfinder/__init__.py
+++ plecfinder/__init__.py
@@ -8,7 +8,7 @@ Identifies plectonemic regions based on the writhe map
 
 from .plecfinder  import find_plecs
 from .plecfinder  import save_topol
-from .plecfinder  import save_topol
+from .plecfinder  import load_topol
 from .plecfinder  import cal_disc_len
 from .plottopol   import plot_topol
 from .state2topol import state2plecs
